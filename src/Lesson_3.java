37ccb038ee5577bb6ae97fa31fa5c4ff7a73b2ce
Alexandr Savko <savkoaleks@gmail.com>
2024-03-12 12:49:26 +0300

    Lesson_3 (5 -10 tasks)

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Lesson_3 {
 
     /**
@@ -45,8 +48,99 @@ public class Lesson_3 {
         }
     }
 
-    public static void main(String[] args) {
-        repeatOutputOfLines("hgif", 3);
+    /**
+     * 5. Написать метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true,
+     * не високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+     */
+    public static void checkLeapYear(int year) {
+        if (year % 4 == 0) {
+            System.out.println("Этот год високосный");
+        } else {
+            System.out.println("Этот год не високосный");
+        }
+    }
+
+    /**
+     * 6. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+     * С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
+    public static void replacingNumbersInArray() {
+        int[] arr = {1, 1, 0, 1, 1, 0, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    /**
+     * 7. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100;
+     */
+    public static void fillingArray() {
+        int[] arr = new int[100];
+        int counter = 1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = counter;
+            counter++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 8. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+     */
+    public static void multiplyingNumbersByTwo() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 9. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+     * и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей,
+     * если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов
+     * равны, то есть [0][0], [1][1], [2][2], ..., [n][n];
+     */
+    public static void fillingArrayDiagonals() {
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == j) {
+                    System.out.print(arr[i][j] = 1);
+                } else {
+                    System.out.print(arr[i][j] = 0);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 10. Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный массив
+     * типа int длиной len, каждая ячейка которого равна initialValue.
+     */
+    public static int[] returnGivenArray(int len, int initialValue){
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++){
+            arr[i] = initialValue;
+        }
+        return arr;
+    }
+
+
+    public static void main(String args[]) {
+        int[] arr1 = returnGivenArray(5, 3);
+        for (int o: arr1) {
+            System.out.print(o + " ");
+        }
     }
 
 }
